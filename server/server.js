bfd6bd28ac2950fe9dd5241942208fdecd667f63
Geert Van Campenhout <vancampenhoutgeert@gmail.com>
2018-02-22 15:40:29 +0700

    Add geolocation support via geolocation api

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
-const {generateMessage} = require('./utils/message');
+const {generateMessage, generateLocationMessage} = require('./utils/message');
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
@@ -15,19 +15,18 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
-  socket.emit('newMsg', generateMessage('Admin', 'Welcome to the chat app'));
+  socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
 
-  socket.broadcast.emit('newMsg', generateMessage('Admin', 'New user joined the channel'));
+  socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined the channel'));
 
-  socket.on('createMsg', (msg, callback) => {
-    console.log('Create new msg:', msg);
-    io.emit('newMsg', generateMessage(msg.from, msg.text));
+  socket.on('createMessage', (message, callback) => {
+    console.log('Create new message:', message);
+    io.emit('newMessage', generateMessage(message.from, message.text));
     callback('This is from the server.');
-    // socket.broadcast.emit('newMsg', {
-    //   from: msg.from,
-    //   text: msg.text,
-    //   createdAt: new Date().getTime()
-    // });
+  });
+
+  socket.on('createLocationMessage', (coords) => {
+    io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
   });
 
   socket.on('disconnect', () => {
--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,14 +1,26 @@
 var expect = require('expect');
 
-var {generateMessage} = require('./message');
+var {generateMessage, generateLocationMessage} = require('./message');
 
 describe('generateMessage', () => {
   it('should generate correct message object', () => {
-    var from = 'geert@example.com'
-    var text = 'Hello you!'
+    var from = 'geert@example.com';
+    var text = 'Hello you!';
     var message = generateMessage(from, text);
     expect(message.from).toBe('geert@example.com');
     expect(message.text).toBe('Hello you!');
     expect(message.createdAt).toBeA('number');
+  });
+});
+
+describe('generateLocationMessage', () => {
+  it('should generate correct location object', () => {
+    var from = 'Admin';
+    var lat = 11;
+    var lon = 21;
+    var url = `https://www.google.com/maps?q=${lat},${lon}`;
+    var location = generateLocationMessage(from, lat, lon);
+    expect(location.createdAt).toBeA('number');
+    expect(location).toInclude({from, url});
   });
 });
