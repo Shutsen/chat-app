9bad103d58329e27690cdcf692e60149a08e377b
Geert Van Campenhout <vancampenhoutgeert@gmail.com>
2018-02-23 11:54:30 +0700

    Format timestamps using momentjs

--- public/js/index.js
+++ public/js/index.js
@@ -8,17 +8,18 @@ socket.on('disconnect', function () {
 });
 
 socket.on('newMessage', function (message) {
-  console.log('New message:', message);
+  var formattedTime = moment(message.createdAt).format('h:mm a');
   var li = $('<li></li>');
-  li.text(`${message.from}: ${message.text}`);
+  li.text(`${message.from} @ ${formattedTime}: ${message.text}`);
   $('#messages').append(li);
 });
 
 // setting it up with li and a vars like this is safe. No one can input malicious code like this.
 socket.on('newLocationMessage', function (message) {
+  var formattedTime = moment(message.createdAt).format('h:mm a');
   var li = $('<li></li>');
   var a = $('<a target="_blank">My current location</a>');
-  li.text(`${message.from}: `);
+  li.text(`${message.from} @ ${formattedTime}: `);
   a.attr('href', message.url);
   li.append(a);
   $('#messages').append(li);
